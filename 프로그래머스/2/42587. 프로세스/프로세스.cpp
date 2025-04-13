ede07d9e8972753f64700ecd4ff76119d69720cb
Sim-Changhyeon <94917106+Sim862@users.noreply.github.com>
2025-04-14 00:34:46 +0900

    [level 2] Title: 프로세스, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,47 +1,33 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <queue>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
+    
+    // 인덱스 관리
     queue<int> processes;
-    for (auto& item : priorities) {
-        processes.push(item);
+    for (int i = 0; i < priorities.size(); i++)
+    {
+        processes.push(i);
     }
 
-    while (true) {
-        bool start = true;
-        int index = 0;
-        for (int i = 0; i < priorities.size(); i++)
-        {
-            if (processes.front() < priorities[i]) {
-                processes.push(processes.front());
-                processes.pop();
-                location--;
-
-                if (location < 0) {
-                    location = processes.size() - 1;
-                }
-
-                start = false;
-                break;
-            }
-            else if (processes.front() == priorities[i]) {
-                index = i;
-            }
+    while (!processes.empty()) {
+        int now_index = processes.front();
+        processes.pop();
+        // 실행 조건 확인
+        if (priorities[now_index] != *max_element(priorities.begin(), priorities.end())) {
+            processes.push(now_index);
         }
-
-        if (start) {
-            processes.pop();
+        else{
             answer++;
-            priorities.erase(priorities.begin() + index);
-            if (location == 0) {
+            priorities[now_index] = -1;
+            // 목표 실행 확인
+            if (location == now_index) {
                 break;
-            }
-            else {
-                location--;
             }
         }
     }
