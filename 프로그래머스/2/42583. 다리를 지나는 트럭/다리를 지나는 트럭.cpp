1f10e03dc3bc23461f64875fd75883ffc920e9c2
Sim-Changhyeon <94917106+Sim862@users.noreply.github.com>
2025-04-14 12:51:13 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.70 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
-    vector<int> truck_cross;
+    queue<int> truck_cross;
 
     int weight_current = 0;
 
@@ -18,24 +18,23 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
 
         if (truck_cross.size() > 0)
         {
-            if (truck_cross[0] <= answer)
+            if (truck_cross.front() <= answer)
             {
                 weight_current -= truck_weights[i - truck_cross.size()];
-                truck_cross.erase(truck_cross.begin());
+                truck_cross.pop();
             }
         }
 
         if (truck_cross.size() < bridge_length) {
 
             if (weight_current + truck_weights[i] <= weight) {
-                truck_cross.push_back(answer + bridge_length);
+                truck_cross.push(answer + bridge_length);
                 weight_current += truck_weights[i];
                 i++;
             }
         }
-
-
     }
-    answer += truck_cross[truck_cross.size()-1] - answer;
+
+    answer += truck_cross.back() - answer;
     return answer;
 }
